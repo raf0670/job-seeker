6517c663cecf357010da3321272f33375c7733e0
raf0670 <swagbondhu@gmail.com>
2026-02-23 21:55:46 +0600

    reset all js

--- js/home.js
+++
@@ -1,9 +0,0 @@
-// Set initial counts based on HTML content
-const initialCount = document.getElementsByClassName("job-cards").length;
-
-if (document.getElementById("available-jobs")) {
-    document.getElementById("available-jobs").innerText = initialCount;
-}
-if (document.getElementById("total-jobs")) {
-    document.getElementById("total-jobs").innerText = initialCount;
-}
